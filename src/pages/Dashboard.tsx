6db6af7b60bec0863fcaa030d3d2167ccfee1109
vale_ <valeria.zerain@gmail.com>
2025-06-01 22:45:38 -0400

    feat:create user card

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import UserCard from "../components/UserCard";
+import { getStorage } from "../helpers/LocalStorage";
+import { getPlaceById } from "../services/Places";
+
 export default function Dashboard() {
+  const user = getStorage("user");
+  const [placeName, setPlaceName] = useState("");
+
+  useEffect(() => {
+    const fetchPlace = async () => {
+        try {
+          const data = await getPlaceById(user.placeId);
+          setPlaceName(data[0]?.name || ""); 
+        } catch (error) {
+          console.error("Error al cargar el recinto", error);
+        }
+    };
+    fetchPlace();
+  }, [user]);
+
+  const labelAndData = [
+    { label: "Nombre(s)", data: user?.name || "" },
+    { label: "Apellido(s)", data: user?.lastName || "" },
+    { label: "Carnet de Identidad", data: user?.ci || "" },
+    { label: "Recinto", data: placeName }
+  ];
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Inicio</h1>
+      <UserCard labelAndData={labelAndData} addPhoto = {true}/>
     </div>
   );
 }
